4a8016c7fa80049db56581e3d9768fa3eac282c5
Cameron W. Barclift <cbarcl01@sunfire31.(none)>
2013-01-31 20:35:16 -0500

    Commit from same session as previous.  Completed add function, need to replace constructor and write copy function.

--- hw_1/TOT2.cpp
+++ hw_1/TOT2.cpp
@@ -1,16 +1,18 @@
 //TOT2.cpp file for homework 1
 
-TOT2::TOT2()
+#include "TOT2.h"
 
+TOT2::TOT2()
 {
-	currentCount = 0;
+	currentCount = 0
 	currentCapacity = INIT_SIZE;
 }
 
 TOT2::TOT2(Transcript* transcriptList, int listSize)
-{
-	
-	currentTranscriptCount = listSize;
+{	
+	currentCount = 0
+	currentCapacity = INIT_SIZE;
+	transcripts = new Transcript*[currentCapacity]
 	
 	for (int i = 0; i < listSize; ++i){
 		transcripts[i] = new Transcript;
@@ -28,14 +30,12 @@ TOT2::TOT2(Transcript* transcriptList, int listSize)
 			}
 		}
 	}
-	
-	
-	
 }
 
 void TOT2::expand()
 {
-	Transcript** temp = new Transcript*[currentCount * RESIZE_FACTOR];
+	Transcript** temp = NULL;
+	temp = new Transcript*[currentCount * RESIZE_FACTOR];
 	
 	for (int i = 0; i < currentCount; ++i){
 		temp[i] = transcripts[i];
@@ -46,3 +46,41 @@ void TOT2::expand()
 	transcripts = temp;
 	temp = NULL;
 }
+bool addTranscript(Transcript* t)
+{
+	if (currentCount == currentCapacity){
+		expand();
+	}
+	
+	pos = currentCount;
+	
+	transcripts[pos] = new Transcript;
+	transcripts[pos]->studentID = t.studentID;
+	transcripts[pos]->numberOfSemesters = .numberOfSemesters;
+	for (int j = 0; j < t->numberOfSemesters; ++j){
+		transcripts[pos]->semesters = new Semester;
+		transcripts[pos]->semesters[j].term = t.semesters[j].term;
+		transcripts[pos]->semesters[j].year = t.semesters[j].year;
+		transcripts[pos]->semesters[j].numberOfCourses = t.semesters[j].numberOfCourses;
+		for (int k = 0; k < t[i].semesters[j].numberOfCourses; ++k){
+			transcripts[pos]->semesters[j].courses = new Course;
+			transcripts[pos]->semesters[j].courses[k].courseNumber = t.semesters[j].courses[k].courseNumber;
+			transcripts[pos]->semesters[j].courses[k].grade = t.semesters[j].courses[k].grade;
+		}
+	}
+	
+	++currentCount
+	return true;
+}
+
+Transcript* getTranscriptCopy(string studentID)
+{
+	
+	
+	
+	
+}
+
+
+
+
