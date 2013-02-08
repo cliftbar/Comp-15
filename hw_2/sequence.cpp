c76cff2f5c5002efff0d9cf4ed07b51661a620e2
cwbarclift <cwbarclift@cwbarclift-mint.(none)>
2013-02-08 01:37:02 -0500

    hw_2 commit 2, did most of it.  Need to write copy constructor and concatenate, which will be easy after copy constructor

--- hw_2/sequence.cpp
+++ hw_2/sequence.cpp
@@ -1,6 +1,7 @@
 
 #include "sequence.h"
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -10,7 +11,7 @@ Sequence_List::Sequence_List()
 	tail = NULL;
 }
 
-Sequence_List::Is_Empty()
+bool Sequence_List::Is_Empty()
 {
 	if(head == NULL){
 		return true;
@@ -20,27 +21,80 @@ Sequence_List::Is_Empty()
 }
 
 //new node added onto the end of the linked list
-Sequence_List::insert(Element_Type value_in)
+void Sequence_List::insert(Element_Type value_in)
 {
-	Node* node_in = new Node;
-	node_in->data = value_in
-	node_in->next = NULL;
+	Node* add_node = new Node;
+	add_node->data = value_in;
+	add_node->next = NULL;
 	
-	if (Is_Empty){
-		head = node_in;
-		tail = node_in
+	if (Sequence_List::Is_Empty()){
+		//cerr << "adding, list empty.\n";//DEBUG CODE
+		head = add_node;
+		tail = add_node;
 		return;
-	}else if(Is_Empty != false){
-		tail->next = node_in;
-		tail = node_in;
+	}else if(!Sequence_List::Is_Empty()){
+		//cerr << "adding, list NOT empty.\n";//DEBUG CODE
+		add_node->next = head;
+		head = add_node;
+		return;
+	}
 }
 
-Sequence_List::concatentation(Sequence_List* list_in)
+int Sequence_List::remove()
+{
+	Node* node_out;
+	
+	if(Sequence_List::Is_Empty()){
+		//cerr << "list empty, no remove.\n";//DEBUG CODE
+		exit(1);
+	}else if(!Sequence_List::Is_Empty()){
+		node_out = head;
+		head = head->next;
+		return node_out->data;
+	}
+	
+return 0;
+}
+
+void Sequence_List::concatenation(Sequence_List list_in)
+{
+	Node* list_in_copy = NULL;
+	Node* current_list = head;
+	
+	//list_in_copy = list_in //make copy constructor work here
+	
+	//tail points to last element
+	//list_in_copy is the head of a dynamic list
+	//tail->next = list_in_copy;
+	
+}
+
+//public print function, just calls priv_print
+void Sequence_List::print()
+{
+	priv_print(head);
+}
+
+//private print function, so I can do recusion and pass it head
+void Sequence_List::priv_print(Node* iter)
+{
+	if(iter == NULL){
+		cout << endl;
+		return;
+	}else if (iter != NULL){
+		cout << iter->data << " ";
+		priv_print(iter->next);
+	}
+	
+return;
+}
+
+//default destructor
+Sequence_List::~Sequence_List()
 {
 	
 }
 
-Sequence_List::remove();
-{
-	
-}
+
+
+
