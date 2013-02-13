b3dc14f6a2daf4f24211d8f36bd8f70e84bbd5ab
cwbarclift <cameron.barclift@tufts.edu>
2013-02-13 04:20:21 -0500

    made the switch to a queue, need comments still

--- hw_2/sequence.cpp
+++ hw_2/sequence.cpp
@@ -24,44 +24,47 @@ bool Sequence_List::Is_Empty()
 Sequence_List::Sequence_List(const Sequence_List &rhs)
 {
 	head = NULL;
+	tail = NULL;
 	copy_lists(rhs.head);
 }
 
 void Sequence_List::copy_lists(Node* h_in)
 {
+
 	Node* copy_node = NULL;
 	
+	if (h_in != NULL){
+		//cout << "head_in: " << h_in->data << endl;
+	}else{
+		//cout << "h_in NULL\n";
+	}
+	
 	if (h_in == NULL){
+		//cout << "tail->data: " << tail->data << endl;
+		tail = NULL;
 		return;
 	}else if (h_in != NULL){
 		copy_node = new Node;
+		//cout << "check2\n";	
 		copy_node->data = h_in->data;
-		copy_node->next = h_in->next;
-		if (head == NULL){
-		head = copy_node;
-		}
-		copy_lists(h_in->next);
-	}
-}
-	
+		
+		if (Is_Empty()){
+			//cout << "head == NULL\n";
+			head = copy_node;
+			tail = copy_node;
+		}else{
 
-//new node added onto the beginning of the linked list
-void Sequence_List::insert(Element_Type value_in)
-{
-	Node* add_node = new Node;
-	add_node->data = value_in;
-	add_node->next = NULL;
-	
-	if (Sequence_List::Is_Empty()){
-		//cerr << "adding, list empty.\n";//DEBUG CODE
-		head = add_node;
-		tail = add_node;
-		return;
-	}else if(!Sequence_List::Is_Empty()){
-		//cerr << "adding, list NOT empty.\n";//DEBUG CODE
-		add_node->next = head;
-		head = add_node;
-		return;
+		//cout << "check1\n";
+		tail->next = copy_node;
+		
+		//cout << "check3\n";	
+		tail = copy_node;
+		
+		}
+		//cout << "head->data: " << head->data << endl;
+		//cout << "copy_node->data: " << copy_node->data << endl;
+		copy_lists(h_in->next);
+
 	}
 }
 
@@ -91,8 +94,12 @@ Element_Type Sequence_List::remove()
 	Node* node_out;
 	
 	if(Sequence_List::Is_Empty()){
-		//cerr << "list empty, no remove.\n";//DEBUG CODE
+		cerr << "list empty, no remove.\n";//DEBUG CODE
 		exit(1);
+	}else if(head == NULL){
+		node_out = head;
+		head = NULL;
+		tail = NULL;
 	}else if(!Sequence_List::Is_Empty()){
 		node_out = head;
 		head = head->next;
@@ -102,13 +109,22 @@ Element_Type Sequence_List::remove()
 return 0;
 }
 
-void Sequence_List::concatenation(const Sequence_List list_in)
+void Sequence_List::concatenation(Sequence_List list_in)
 {
-	Sequence_List list_in_copy = list_in;
+	//cout << "list_in_copy = list_in\n";
+	//Sequence_List list_in_copy = list_in;
 	
-	while (!list_in_copy.Is_Empty()){
-		insert(list_in_copy.remove());
-	}	
+	//cout << "list_in_copy\n";
+	//list_in_copy.print();
+	
+	while (!list_in.Is_Empty()){
+		//cout << "list_in_copy\n";
+		//cout << "list_in print: ";
+		//list_in.print();
+		insert(list_in.remove());
+		//cout << "print: ";
+		//print();
+	}
 }
 
 //public print function, just calls priv_print
