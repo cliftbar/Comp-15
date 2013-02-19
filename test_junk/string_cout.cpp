54540ba1ee38fc78c724c23ea37b7a7d58355e26
cwbarclift <cameron.barclift@tufts.edu>
2013-02-19 00:50:37 -0500

    started project 1 header files

---
+++ test_junk/string_cout.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+	string junk1;
+	string junk2;
+	int junk3;
+
+	cin >> junk1;
+	cin >> junk2;
+	cin >> junk3;
+
+	cout << junk1 << endl;
+	cout << junk2 << endl;
+	cout << junk3 << endl;
+
+return 0;
+}
