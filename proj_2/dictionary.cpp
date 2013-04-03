ae6c911976c6790a1ccb10c4dee30fe2356ee705
cwbarclift <cwbarclift@cwbarclift-mint.(none)>
2013-04-03 00:45:56 -0400

    I made proj 2 pretty

--- proj_2/dictionary.cpp
+++ proj_2/dictionary.cpp
@@ -1,13 +1,20 @@
-//dictionary.cpp
+//dictionary.h
+//Created by: Cameron Barclift
+//Class contains the functions neccessary for the operation of a dictionary
+//using a trie data structure.  Allows for words to be stored, can print all
+//words stored, and can look up whether a specific word is stored and if a
+//specific string is a prefix of a word.  Dictionary contains only
+//alphabetical characters; any characters that are not alphabetical are
+//removed from the input string, which is then stored, even if it is empty
 
 #include "dictionary.h"
 #include "node.h"
-#include <iostream>
 using namespace std;
 
 /////////////////////////////Public Methods///////////////////////////////////
 
-//constructor
+//Default constructor, intialized s_curr to an empty string, root to NULL, and
+//curr_s_pos to zero.
 Dictionary::Dictionary()
 {
 	s_curr = "";
@@ -15,33 +22,39 @@ Dictionary::Dictionary()
 	root = NULL;
 }
 
+//Public insert funciton, calls the word clean up fucntion and passes the
+//cleaned string to the recursive insert funciton.  It also creates a new
+//for root if the dictionary is empty.  Returns false if out of memory.
 bool Dictionary::insert(string s)
 {
-	//cout << "word in: " << s << endl;//DEBUG CODE
 	s_curr = clean_word(s);
-	//cout << "word cleaned: " << s_curr << endl;//DEBUG CODE
 	
 	if(root == NULL){
 		root = new Node;
 	}
 	
-	rec_insert(root);
-	
-	return true;
+	return rec_insert(root);
 }
 
+//Public isPrefix function, calls the word clean up function and passes the
+//cleaned string to the recursive find path function with the prefix switch 
+//value set to true. Function returns true if the word is a prefix.
 bool Dictionary::isPrefix(string s)
 {
 	s_curr = clean_word(s);
-	return rec_string_find(root, true);
+	return rec_find_path(root, true);
 }
 
+//Public isWord function, calls the word clean up function and passes the
+//cleaned string to the recursive find path function with the prefix switch 
+//value set to false. Function returns true if the word is in the dictionary.
 bool Dictionary::isWord(string s)
 {
 	s_curr = clean_word(s);
-	return rec_string_find(root, false);
+	return rec_find_path(root, false);
 }
 
+//Public print all function, prints every word in the dictionary
 void Dictionary::print_all_words()
 {
 	string print_string = "";
@@ -51,6 +64,9 @@ void Dictionary::print_all_words()
 
 ///////////////////////////Private Methods////////////////////////////////////
 
+//Function removes any non-alphabetical characters from the input string, and
+//returns a string that contains the remaining alphabetical characters from  
+//the input string.
 string Dictionary::clean_word(string s_in)
 {
 	string r_string = "";
@@ -64,93 +80,88 @@ string Dictionary::clean_word(string s_in)
 	return r_string;
 }
 
+//returns an integer from 0-25 that represents the trie array postion of the
+//character passed in
 int Dictionary::alpharray_pos(char c_in)
 {
 	return (c_in - 'a');
 }
 
-void Dictionary::rec_insert(Node* np_in)
+//Recursive insert function called by public insert function.  Returns false
+//if there is no more memory
+bool Dictionary::rec_insert(Node* np_in)
 {
 	int curr_elm = alpharray_pos(s_curr[curr_s_pos]);
-	//cout << "curr_s_pos: " << curr_s_pos << endl;//DEBUG CODE
-	//cout << "s_curr.length(): " << s_curr.length() << endl;//DEBUG CODE
-	//cout << "s_curr: " << s_curr << endl;//DEBUG CODE
-	//cout << "curr_elm: " << curr_elm << endl;//DEBUG CODE
 	
 	if(curr_s_pos >= s_curr.length()){
-		//cout << "check1" << endl;//DEBUG CODE
 		np_in->set_word_here(true);
 		curr_s_pos = 0;
-		return;
-	}else if(np_in->alpharray[curr_elm] == NULL){
-		//cout << "check2" << endl;//DEBUG CODE
-		np_in->alpharray[curr_elm] = new Node;
-		if (np_in->alpharray[curr_elm] != NULL){
+		return true;
+	}else{
+		if(np_in->array_access(curr_elm) == NULL){
+			np_in->make_node(curr_elm);
+		}
+		if (np_in->array_access(curr_elm) != NULL){
 			++curr_s_pos;
-			rec_insert(np_in->alpharray[curr_elm]);
-			return;
+			return rec_insert(np_in->array_access(curr_elm));
 		}else {
 			cerr << "ERROR: Out of memory.  Program exit" << endl;
 			exit(1);
+			//return false;
 		}
-	}else if(np_in->alpharray[curr_elm] != NULL){
-		//cout << "check3" << endl;//DEBUG CODE
-		++curr_s_pos;
-		rec_insert(np_in->alpharray[curr_elm]);
-		return;
 	}
 }
 
-bool Dictionary::rec_string_find(Node* np_in, bool prefix)
+//Recursive function called by isPrefix and isWord.  The fucntion follows
+//the path set by the current string, and checks for prefixes if the switch
+//value "prefix" is true, and for whole words when "prefix" is false.  
+//Returns false if the input string is not a prefix or word
+bool Dictionary::rec_find_path(Node* np_in, bool prefix)
 {
 	int curr_elm = alpharray_pos(s_curr[curr_s_pos]);
-	//cout << "curr_s_pos: " << curr_s_pos << endl;//DEBUG CODE
-	//cout << "s_curr.length(): " << s_curr.length() << endl;//DEBUG CODE
-	//cout << "s_curr: " << s_curr << endl;//DEBUG CODE
-	//cout << "curr_elm: " << curr_elm << endl;//DEBUG CODE
 	
-	if(curr_s_pos >= s_curr.length()){
+	if (np_in == NULL){
+		return false;
+	}else if(curr_s_pos >= s_curr.length()){
 		if (prefix){
 			curr_s_pos =0;
-			//cout << "check5" << endl;//DEBUG CODE
 			return true;
 		}else {
 			if (np_in->word_here()){
-				//cout << "check6" << endl;//DEBUG CODE
 				curr_s_pos =0;
 				return true;
 			}else {
-				//cout << "check7" << endl;//DEBUG CODE
 				curr_s_pos =0;
 				return false;
 			}
 		}
-	}else if(np_in->alpharray[curr_elm] == NULL){
-		//cout << "check8" << endl;//DEBUG CODE
+	}else if(np_in->array_access(curr_elm) == NULL){
 		curr_s_pos = 0;
 		return false;
 	}else {
-		//cout << "check9" << endl;//DEBUG CODE
 		++curr_s_pos;
-		return rec_string_find(np_in->alpharray[curr_elm], prefix);
+		return rec_find_path(np_in->array_access(curr_elm), prefix);
 	}
 }
 
+//Recursive print function.  
 void Dictionary::rec_print(Node* np_in, string print_string)
 {
 	string curr_path = print_string;
-	if (np_in->word_here()){
-		cout << print_string << endl;
-	}
 	
-	for (int i =0; i < 26; ++i){
-		print_string = curr_path;
-		if (np_in->alpharray[i] != NULL){
-			//cout << "check4" << endl;//DEBUG CODE
-			print_string += (i + 'a');
-			rec_print(np_in->alpharray[i], print_string);
-		}else if (np_in->alpharray == NULL){
-			return;
+	if (np_in == NULL){
+		cout << "Empty Dictionary" << endl;
+	} else {
+		if (np_in->word_here()){
+			cout << print_string << endl;
+		}
+		
+		for (int i =0; i < 26; ++i){
+			print_string = curr_path;
+			if (np_in->array_access(i) != NULL){
+				print_string += (i + 'a');
+				rec_print(np_in->array_access(i), print_string);
+			}
 		}
 	}
 }
--- proj_2/node.cpp
+++ proj_2/node.cpp
@@ -1,26 +1,46 @@
-//node.cpp
+//node.h
+//Created by: Cameron Barclift
+//Node class for dictionary.  The length of the array is set to 26 characters 
+//(the length of the alphabet).  array_access() and make_node() allow indirect 
+//access to the array, and set_word_here() and word_here() allow indirect
+//access to is_word.  
 
 #include "node.h"
 
 /////////////////////////////Public Methods///////////////////////////////////
+
+//Default constructor, intializes every element of the array to NULL, sets
+//is_word to false
 Node::Node()
 {
-	ALPHA_LENGTH = 26;
-	for (int i = 0; i < ALPHA_LENGTH; ++i){
+	for (int i = 0; i < 26; ++i){
 		alpharray[i] = NULL;
 	}
 	
 	is_word = false;
 }
 
+//Returns the pointer contained in the array at the position passed in
+Node* Node::array_access(int pos)
+{
+	return alpharray[pos];
+}
+
+//makes a new node and stores the address in the array
+void Node::make_node(int pos)
+{
+	alpharray[pos] = new Node;
+}
+
+//Returns the value of is_word
 bool Node::word_here()
 {
 	return is_word;
 }
 
+//Sets the value of is_word to the value passed in
 void Node::set_word_here(bool setter)
 {
 	is_word = setter;
 }
 
-//Node::insert(string str_insert){
